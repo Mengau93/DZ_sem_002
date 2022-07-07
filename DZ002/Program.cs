a853b2b0e53bed05f092e14fbb4baa387ebc5e09
Romka <android91@mail.ru>
2022-07-07 15:24:56 +0300

    add DZ 001 add DZ 002 add DZ 003

---
+++ DZ002/Program.cs
@@ -0,0 +1,14 @@
+﻿// // Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+
+Console.Write("Введи число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+string numbers = Convert.ToString(N);
+if (numbers.Length > 2)
+{
+    Console.WriteLine("Третья цифра: " + numbers[2]);
+}
+else
+{
+    Console.WriteLine("Третьей цифры нет");
+}
